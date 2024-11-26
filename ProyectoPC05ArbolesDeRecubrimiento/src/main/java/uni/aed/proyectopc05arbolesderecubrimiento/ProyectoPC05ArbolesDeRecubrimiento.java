d058dfc72a37fc48baa62ddfe4e31c686b4a8f20
MjsaMax <137005494+MjsaMax@users.noreply.github.com>
2024-11-26 12:08:31 -0500

    Add files via upload
    Creacion del proyecto

---
+++ ProyectoPC05ArbolesDeRecubrimiento/src/main/java/uni/aed/proyectopc05arbolesderecubrimiento/ProyectoPC05ArbolesDeRecubrimiento.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package uni.aed.proyectopc05arbolesderecubrimiento;
+
+/**
+ *
+ * @author Max--
+ */
+public class ProyectoPC05ArbolesDeRecubrimiento {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
