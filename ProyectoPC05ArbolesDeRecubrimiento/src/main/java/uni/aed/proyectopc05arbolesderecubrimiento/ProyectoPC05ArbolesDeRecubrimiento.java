a3f90f0bdb68b0b2eddd52ff2f688f77394c206d
MjsaMax <mjsamax@gmail.com>
2024-11-26 16:29:42 -0500

    Agregue un testeo

--- ProyectoPC05ArbolesDeRecubrimiento/src/main/java/uni/aed/proyectopc05arbolesderecubrimiento/ProyectoPC05ArbolesDeRecubrimiento.java
+++ ProyectoPC05ArbolesDeRecubrimiento/src/main/java/uni/aed/proyectopc05arbolesderecubrimiento/ProyectoPC05ArbolesDeRecubrimiento.java
@@ -12,5 +12,7 @@ public class ProyectoPC05ArbolesDeRecubrimiento {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Preuba");
+        System.out.println("Testeo");
     }
 }
