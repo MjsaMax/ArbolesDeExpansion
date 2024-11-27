9ff30757703918a86c5b4ad7daf79432e809d1b7
MjsaMax <mjsamax@gmail.com>
2024-11-27 11:14:30 -0500

    Se integra la clase Graph

---
+++ src/main/java/uni/aed/arbolesdeexpansion/Graph.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uni.aed.arbolesdeexpansion;
+import java.util.*;
+/**
+ *
+ * @author Max--
+ */
+public interface Graph {
+    void addEdge(int src, int dest, int weight);
+    List<Edge> getEdges();
+    int getVertexCount();
+}
