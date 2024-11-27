27085f9e78e03446f93e0a0b1d7488a969d3c400
MjsaMax <mjsamax@gmail.com>
2024-11-27 11:14:07 -0500

    Se integra la clase Edge

---
+++ src/main/java/uni/aed/arbolesdeexpansion/Edge.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uni.aed.arbolesdeexpansion;
+import java.util.*;
+/**
+ *
+ * @author Max--
+ */
+// Edge class to represent graph edges
+public class Edge implements Comparable<Edge> {
+    int src, dest, weight;
+
+    public Edge(int src, int dest, int weight) {
+        this.src = src;
+        this.dest = dest;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Edge other) {
+        return this.weight - other.weight;
+    }
+}
