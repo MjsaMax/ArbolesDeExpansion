aa6ac36e75831ed4be89960dced22959ef176665
MjsaMax <mjsamax@gmail.com>
2024-11-27 11:09:13 -0500

    He generado un ejemplo de kruskal

--- src/main/java/uni/aed/arbolesdeexpansion/ArbolesDeExpansion.java
+++ src/main/java/uni/aed/arbolesdeexpansion/ArbolesDeExpansion.java
@@ -3,20 +3,26 @@
  */
 
 package uni.aed.arbolesdeexpansion;
+import java.util.*;
 
 /**
  *
  * @author Max--
  */
 public class ArbolesDeExpansion {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Final");
-        
-        // Esto es una prueba MrMauro
-        System.out.println("Esto es una prueba");
-        
-        //segunda prueba
+        Graph graph = new SimpleGraph(4);
+        graph.addEdge(0, 1, 10);
+        graph.addEdge(0, 2, 6);
+        graph.addEdge(0, 3, 5);
+        graph.addEdge(1, 3, 15);
+        graph.addEdge(2, 3, 4);
+
+        List<Edge> mst = Kruskal.findMST(graph);
+
+        System.out.println("Edges in the Minimum Spanning Tree:");
+        for (Edge edge : mst) {
+            System.out.println(edge.src + " - " + edge.dest + " : " + edge.weight);
+        }
     }
 }
