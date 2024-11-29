764aed8d98f5ff588a6dd6a30a9a8a52ee3b4db1
MjsaMax <mjsamax@gmail.com>
2024-11-29 14:58:29 -0500

    Implementacion de Linkedlist y iterable visto en clase

--- src/main/java/uni/aed/arbolesdeexpansion/ArbolesDeExpansion.java
+++ src/main/java/uni/aed/arbolesdeexpansion/ArbolesDeExpansion.java
@@ -1,14 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package uni.aed.arbolesdeexpansion;
-import java.util.*;
+import uni.aed.tda.linkedlistTDA.Iterador;
+import uni.aed.tda.linkedlistTDA.LinkedListTDA;
 
-/**
- *
- * @author Max--
- */
 public class ArbolesDeExpansion {
     public static void main(String[] args) {
         Graph graph = new SimpleGraph(4);
@@ -18,11 +11,14 @@ public class ArbolesDeExpansion {
         graph.addEdge(1, 3, 15);
         graph.addEdge(2, 3, 4);
 
-        List<Edge> mst = Kruskal.findMST(graph);
+        LinkedListTDA<Edge> mst = Kruskal.findMST(graph);
 
         System.out.println("Edges in the Minimum Spanning Tree:");
-        for (Edge edge : mst) {
+        Iterador<Edge> edgeIterator = new Iterador<>(mst);
+        while (edgeIterator.hasNext()) {
+            Edge edge = edgeIterator.next();
             System.out.println(edge.src + " - " + edge.dest + " : " + edge.weight);
         }
     }
 }
+
