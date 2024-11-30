04775c02002b612d82066514c3877f5fa4667d2d
bgalium <walter.poma.n@uni.pe>
2024-11-30 12:26:43 -0500

    probando push walter clase prueba

---
+++ src/main/java/uni/aed/proyectofinal/prueba.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uni.aed.proyectofinal;
+
+/**
+ *
+ * @author Walter
+ */
+public class prueba {
+    public void main(String[] args){
+        System.out.print("modificando");
+    }
+}
